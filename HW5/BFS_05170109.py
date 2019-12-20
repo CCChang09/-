49f4dd2d3a5b90a749a07410d68c55ef53108f32
CCChang09 <55650886+CCChang09@users.noreply.github.com>
2019-12-20 19:52:19 +0800

    Update BFS_05170109.py

--- HW5/BFS_05170109.py
+++ HW5/BFS_05170109.py
@@ -4,6 +4,25 @@
 # In[ ]:
 
 
+class Node:
+    def _init_(self,val):
+        self.val = val
+        self.next = None
+        
+class Stack:
+    def _init_(self):
+        self.head = None
+        self.size = 0
+        
+    def insert(self,k):
+        if self.head != None:
+            for i in range(self.size-1):
+                self.head = self.head.next
+            self.head.next = Node(k)
+            
+        if self.head == None:
+            self.head = Node(k)
+        
 class Graph:
     def _init_(self):
         self.graph = defaultdict(list)
